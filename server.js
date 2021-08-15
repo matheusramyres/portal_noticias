fc2f726a592b0ac6a4c3ee42bdf3cd2bc53693b5
Matheus Ramyres <matheus63360@gmail.com>
2021-08-15 16:06:10 -0300

    update server

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require("express");
-const expressValidator = require("express-validator");
 const routes = require("./src/routes/routes");
+
+
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "src/views");
+
+app.use(express.static('./src/public'));
+app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(expressValidator());
+
 app.use(routes);
 
+
+
 app.listen(3000, ()=>{
-    console.log("Servidor rodando na porta 3000");
+    console.log("Servidor rodando no link: http://localhost:3000");
 });
